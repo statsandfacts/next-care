2590a0e2c9413bec26d5ec886fdbc5629d02567c
sambeet0809 <mandalsambeet94@gmail.com>
2024-03-22 20:09:40 +0530

    cors config

--- one-health/main.py
+++ one-health/main.py
@@ -9,11 +9,20 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
